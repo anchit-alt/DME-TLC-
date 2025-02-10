a3e27061c731522532d0a7d2a146985dd5442ef9
Anchit <83338676+anchit-alt@users.noreply.github.com>
2025-02-11 02:07:29 +0530

    Frontend for Spur and Bevel Gear. Home Button modifications, and multiple return statements

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ def delay_decorator(function):
     return wrapper_function
 
 
+axial = 0
+radial = 0
 @app.route("/")
 def home():
     # template_path = os.path.abspath("/Users/anchit/Documents/GitHub/DME-TLC-/Codes/templates/index.html")
@@ -29,13 +31,198 @@ def home():
 @app.route("/chapter_14.html",methods = ["POST","GET"])
 def chapter_14():
     if request.method == "POST":
-        return "Hi"
+        type_of_gear = request.form.get("type_of_gear")
+        pressure_angle = float(request.form.get("pressure_angle"))
+        number_of_teeth = float(request.form.get("number_of_pinion"))
+        Number_of_teeth_gear = float(request.form.get("number_of_gear"))
+        Module = float(request.form.get("module"))
+        Transmitted_power_by_pinion = float(request.form.get("power_by_Pinion"))
+        Pinion_speed = float(request.form.get("pinion_speed"))
+        Grade = request.form.get("grade")
+        Face_width = float(request.form.get("face_width"))
+        Hbp = float(request.form.get("pinion_hardness"))
+        Hbg = float(request.form.get("gear_hardness"))
+        Crowned_Uncrowned = request.form.get("crowned/uncrowned")
+        Quality_standard_number = float(request.form.get("quality_standard_number"))
+        Pinion_life_in_no_of_cycles = float(request.form.get("pinion_number_of_cycles"))
+        reliability = float(request.form.get("reliability"))
+        gear_condition = request.form.get("gear_condition")
+        for_ce = request.form.get("assembly/other")
+        external_or_internal_gear = request.form.get("external/internal")
+        power_source = request.form.get("power_source")
+        driven_machine = request.form.get("driven_machine")
+        if Crowned_Uncrowned == "crowned":
+            cmc = 0.8
+        elif Crowned_Uncrowned =="uncrowned":
+            cmc = 1
+        pitch_dia_pinion = Module * number_of_teeth
+        pitch_dia_gear = Module * Number_of_teeth_gear
+        pitch_line_velocity = float((math.pi * pitch_dia_pinion * Pinion_speed)/ 60)
+        load = float(Transmitted_power_by_pinion / pitch_line_velocity)
+        b = float(0.25*(12-Quality_standard_number)**(2/3))
+        A = 50 + 56*(1-b)
+        Kv = float((A+math.sqrt(200*pitch_line_velocity)/(A))**b)
+        data = pd.read_csv("Data/teeth_vs_y.csv")
+        data['Number_of_Teeth'] = pd.to_numeric(data['Number_of_Teeth'])
+        data['Y'] = pd.to_numeric(data['Y'])
+
+        def interpolate_y(number_of_teeth, data):
+            teeth = data['Number_of_Teeth'].values
+            y_values = data['Y'].values
+
+            if number_of_teeth < teeth.min() or number_of_teeth > teeth.max():
+                raise ValueError("Number of teeth is outside the range of available data.")
+
+
+            for i in range(len(teeth) - 1):
+                if teeth[i] <= number_of_teeth <= teeth[i + 1]:
+                    
+                    x1, x2 = teeth[i], teeth[i + 1]
+                    y1, y2 = y_values[i], y_values[i + 1]
+                    interpolated_y = y1 + (y2 - y1) * ((number_of_teeth - x1) / (x2 - x1))
+                    return interpolated_y
+
+            raise ValueError("Interpolation failed. Check the input data.")
+
+        interpolated_y_pinion = interpolate_y(number_of_teeth, data)
+        print(f"Interpolated Y for {number_of_teeth} teeth: {interpolated_y_pinion}")
+
+        number_of_teeth = Number_of_teeth_gear
+        interpolated_y_gear = interpolate_y(number_of_teeth,data)
+        print(f"Interpolated Y for {number_of_teeth} teeth: {interpolated_y_gear}")
+
+
+
+        Ks_p = 0.8433 * (Module * Face_width * math.sqrt(interpolated_y_pinion)**0.0535)
+        Ks_g = 0.8433 * (Module * Face_width * math.sqrt(interpolated_y_gear)**0.0535)
+
+        if Face_width <=25:
+            cpf = (Face_width/(10*pitch_dia_pinion)) - 0.025 
+        elif Face_width > 25 and Face_width <=425:
+            (Face_width/(10*pitch_dia_pinion)) - 0.0375 +4.92*pow(10,-4)*Face_width
+        elif Face_width >425 and Face_width <=1000:
+            (Face_width/(10*Face_width)) - 0.1109 +8.15*pow(10,-4)*Face_width - 3.53*pow(10,-7)*Face_width**2
+
+        cpm = 1 
+        gear_conditions = pd.read_csv("Data/corrected_gear_conditions.csv")
+        if gear_condition == "O":
+            A_gear = 0.247
+            B_gear = 0.0167
+            C_gear = -0.765 * pow (10,-4)
+        elif gear_condition == "C":
+            A_gear = 0.127
+            B_gear = 0.0158
+            C_gear = -0.930 * pow(10,-4)
+        elif gear_condition == "P":
+            A_gear = 0.0675
+            B_gear = 0.0128
+            C_gear = -0.926 * pow(10,-4)
+        elif gear_condition == "E":
+            A_gear = 0.00360
+            B_gear = 0.0102
+            C_gear = -0.822 * pow(10,-4)
+
+        Face_width_in_inch = Face_width / 12
+
+        cma = A_gear + B_gear*Face_width_in_inch + C_gear*pow(Face_width_in_inch,2)
+
+        if for_ce == "assembly":
+            ce = 0.8
+        elif for_ce =="other":
+            ce =1 
+        Kh = 1 + cmc * (cpf*cpm + cma * ce)
+
+        N = Pinion_life_in_no_of_cycles
+        Yn_pinion = 1.3388 * pow(N,-0.0178)
+        Yn_gear = 1.388 * pow((N / (Number_of_teeth_gear/number_of_teeth)),-0.0178)
+        ################################
+        Yj_p = 0.33
+        Yj_g = 0.38 
+        ################################
+        if reliability > 0.5 and reliability < 0.99:
+            Yz = 0.658 - 0.0759*math.log(1-reliability)
+        elif reliability >= 0.99 and reliability <= 0.9999:
+            Yz = 0.50 - 0.109*math.log(1-reliability)
+
+        mn = 1
+        gear_ratio = float(Number_of_teeth_gear/number_of_teeth)
+
+        if external_or_internal_gear =="external":
+            zi = (math.cos(pressure_angle)*math.sin(pressure_angle)*gear_ratio)/(2*mn*(gear_ratio+1))
+        elif external_or_internal_gear=="internal":
+            zi = (math.cos(pressure_angle)*math.sin(pressure_angle)*gear_ratio)/(2*mn*(gear_ratio-1))
+        Ze = 191 * math.sqrt(pow(10,6))
+
+        if Grade == "1":
+            Stp = 0.553 * Hbp + 88.3 
+            Stg = 0.553 * Hbg + 88.3
+        elif Grade == "2":
+            Stp = 0.703 * Hbp + 113
+            Stg = 0.703 * Hbg + 113
+
+        if Grade == "1":
+            Scp = 0.568 * Hbp + 83.8
+            Scg = 0.568 * Hbg + 83.8
+        elif Grade == "2":
+            Scp = 0.749 * Hbp + 110
+            Scg = 0.749 * Hbg + 110
+
+        Zn = 1.4488 * pow(N,-0.023)
+        if float(Hbp/Hbg) <1.2:
+            Ch = 1
+
+        if power_source == "U":
+            if driven_machine == "U":
+                Ko =1
+            if driven_machine == "ModS":
+                Ko = 1.25
+            if driven_machine == "HS":
+                Ko =1.75
+        if power_source == "LS":
+            if driven_machine == "U":
+                Ko =1.25
+            if driven_machine == "ModS":
+                Ko = 1.5
+            if driven_machine == "HS":
+                Ko =2
+        if power_source == "MS":
+            if driven_machine == "U":
+                Ko =1.5
+            if driven_machine == "ModS":
+                Ko = 1.75
+            if driven_machine == "HS":
+                Ko =2.25
+        Kb = 1 
+        Y_thetha = 1
+
+        sigma_p = (load  * Ko * Kv * Ks_p * Kh * Kb)/(Face_width * Module * Yj_p)
+        sigma_g= (load  * Ko * Kv * Ks_g * Kh * Kb)/(Face_width * Module * Yj_g)
+
+        Sf_p = (Stp * Yn_pinion)/(sigma_p*Y_thetha*Yz)
+        Sf_g = (Stg * Yn_gear)/(sigma_g*Y_thetha*Yz)
+        Zr = 1
+        sigma_c_p = Ze * math.sqrt((load*Ko*Kv*Ks_p*Kh*Zr)/(pitch_dia_pinion*Face_width*zi))
+        sigma_c_g = Ze * math.sqrt((load*Ko*Kv*Ks_g*Kh*Zr)/(pitch_dia_gear*Face_width*zi))
+
+        print("sigma_c_g", sigma_c_g)
+        print("sigma_c_p",sigma_c_p)
+        Zw = 1
+        Sh_p = (Scp*Zn*Zw)/(sigma_c_p * Y_thetha * Yz)
+        Sh_g = (Scg*Zn*Zw)/(sigma_c_g * Y_thetha * Yz)
+
+        print("Sh_p",Sh_p)
+        print("Sh_g",Sh_g)
+        return f'''<p> sigma_c_g: {round(sigma_c_g,3)}</p><br><p>igma_c_p: {round(sigma_c_p,3)}</p><br><p>Sh_p: {round(Sh_p,9)}</p><br><p>Sh_g: {round(Sh_g,9)}</p><br><p>Sigma Pinion: {round(sigma_p,9)}</p><br><p>Sigma gear: {round(sigma_g,3)}</p><br><p>Sf_p: {round(Sf_p,3)}</p><br><p>Sf_g: {round(Sf_g,3)}</p>
+            <a href="/"><button>Return to Home</button></a>
+            '''
     else:
         return render_template("chapter_14.html")
 
 @app.route("/bearing_selection.html",methods = ["POST","GET"])
 def select():
     if request.method == "POST":
+        global axial
+        global radial
         axial = float(request.form.get('ax_load'))
         radial = float(request.form.get('r_load'))
         if axial <= 0.05*radial:
@@ -79,8 +266,12 @@ def ball_bearing():
             V = 1.2
         
         af = float(request.form.get('af')) or int(request.form.get('af'))
-        Fa = float(request.form.get("Axial_load"))
-        Fr = float(request.form.get("Radial_load"))
+        Fa = axial
+        Fr = radial
+        print("Fa",axial)
+        print("Fr",radial)
+        # Fa = float(request.form.get("Axial_load"))
+        # Fr = float(request.form.get("Radial_load"))
         ld = float(request.form.get("desired_life"))
         nd = float(request.form.get("desired_speed"))
 
@@ -173,7 +364,9 @@ def ball_bearing():
         # iteration(X,Y)
         selected_bore = iteration(X,Y)
         if selected_bore:
-            return f"Select bore of: {selected_bore} mm"
+            return f'''<p>Select bore of: {selected_bore} mm</p>
+            <a href="/"><button>Return to Home</button></a>
+            '''
         else:
             return "Unable to select a suitable bore, possibly the diameter exceeds 95 mm"
     else:
@@ -197,8 +390,14 @@ def cylindrical_bearing():
                 V = 1.2
 
             af = float(request.form.get('af') or 0)  # Default to 0 if not provided
-            Fa = float(request.form.get("Axial_load") or 0)
-            Fr = float(request.form.get("Radial_load") or 0)
+            global axial
+            global radial
+            Fa = axial
+            Fr = radial
+
+            print("Fa and Fr are ",Fa,Fr)
+            # Fa = float(request.form.get("Axial_load") or 0)
+            # Fr = float(request.form.get("Radial_load") or 0)
             ld = float(request.form.get("desired_life") or 0)
             nd = float(request.form.get("desired_speed") or 0)
 
@@ -263,7 +462,9 @@ def cylindrical_bearing():
 
             selected_bore = iteration(X, Y)
             if selected_bore:
-                return f"Select bore of: {selected_bore} mm"
+                return f'''<p>Select bore of: {selected_bore} mm</p>
+            <a href="/"><button>Return to Home</button></a>
+            '''
             else:
                 return "Unable to select a suitable bore, possibly the diameter exceeds 95 mm"
         except Exception as e:
@@ -776,7 +977,8 @@ def page1():
 
         #test run
         result = process_loading_option()
-        return f"Answer :  {result}"
+        return f'''Answer :  {result}
+                <a href="/"><button>Return to Home</button></a>'''
         
     else:
         return render_template("fos.html")
